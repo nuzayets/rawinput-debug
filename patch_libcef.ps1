dada206e78658ebe029c29b920c91a7d1f56b754
Dmitry Safonov <dmitry@dsafonov.com>
2020-12-06 17:58:15 -0500

    Tested against 3.20.5.83

--- patch_libcef.ps1
+++ patch_libcef.ps1
@@ -1,14 +1,15 @@
 ## Make sure NVIDIA GeForce Experience overlay is disabled before running this script.
 ## PowerShell should be started as Administrator.
-## This was tested against GeForce Experience 3.20.5.70. libcef might not change that frequently.
-## Your mileage may vary!
+## This was tested against GeForce Experience:
+##  - 3.20.5.70
+##  - 3.20.5.83
 
 $path = Split-Path ((Get-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\GeForceExperience\Shell\Open\Command).'(default)' -replace '"',"")
 $file = [System.IO.File]::ReadAllBytes("$path\libcef.dll")
 $offset = 0x61e0ae8
 if($file[$offset] -eq 4 -and $file[$offset+2] -eq 5 -and $file[$offset+4] -eq 8) {
-	Stop-Process -Name "NVIDIA GeForce Experience"
-	Stop-Process -Name "NVIDIA Share"
+	Stop-Process -Name "NVIDIA GeForce Experience" -ErrorAction SilentlyContinue
+	Stop-Process -Name "NVIDIA Share" -ErrorAction SilentlyContinue
 	[System.IO.File]::WriteAllBytes("$path\libcef.dll.bak", $file)
 	$file[$offset] = 6
 	[System.IO.File]::WriteAllBytes("$path\libcef.dll", $file)
